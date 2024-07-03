003423dfed4293c40b5ecf198dc03ecf694399a9
Akhil Kumar <akhilkumar2001@gmail.com>
2024-07-03 19:10:54 +0530

    fea: Similar Products done

--- controller/userController/productController.js
+++ controller/userController/productController.js
@@ -7,8 +7,13 @@ const productDetail = async(req,res) =>{
     try {
         const id = req.params.id
         const product = await productSchema.findById(id)
-        const similarProduct = await productSchema.find({ productCategory: product.productCategory, isActive: true })
-        res.render('user/productDetail',{ title: product.productName,alertMessage:req.flash('alert'), product, similarProduct, user:req.session.user })
+        if(product.isActive == true){
+            const similarProduct = await productSchema.find({ productCategory: product.productCategory, isActive: true })
+            res.render('user/productDetail',{ title: product.productName,alertMessage:req.flash('alert'), product, similarProduct, user:req.session.user })
+        }else{
+            req.flash('alert','Product not found')
+            res.redirect('/home')
+        }
     } catch (error) {
         console.log(`Error while rendering product page ${error}`)
     }
